c69a5d9acc6b10a4929f5aa3afd0177aac3e00e4
Andrew Moss <andrew@m0ss.dev>
2021-05-15 15:15:37 -0600

    fix: Removal of async dep in build

--- lib/winston-azure-blob.ts
+++ lib/winston-azure-blob.ts
@@ -1,57 +1,73 @@
-import Transport from 'winston-transport'
-import async from 'async'
-import { BlobService, createBlobService, createBlobServiceWithSas, StorageError } from 'azure-storage'
-import moment from 'moment'
-import { MESSAGE } from 'triple-beam'
-import Debug from 'debug'
+import Transport from "winston-transport";
+import { eachSeries } from "async";
+import {
+  BlobService,
+  createBlobService,
+  createBlobServiceWithSas,
+  StorageError,
+} from "azure-storage";
+import moment from "moment";
+import { MESSAGE } from "triple-beam";
+import Debug from "debug";
 
-const debug = Debug('winston-azure-blob')
-const MAX_APPEND_BLOB_BLOCK_SIZE = 4 * 1024 * 1024
+const debug = Debug("winston-azure-blob");
+const MAX_APPEND_BLOB_BLOCK_SIZE = 4 * 1024 * 1024;
 
 /**
  * Azure storage account credential variants
  */
-type Account = { name: string, key: string } | { host: string, sasToken: string }
+type Account =
+  | { name: string; key: string }
+  | { host: string; sasToken: string };
 
 interface IWinstonAzureBlob {
-  account: Account
-  azBlobClient: BlobService
-  containerName: string
-  blobName: string
-  rotatePeriod: string
-  eol: string
-  bufferLogSize: number
-  syncTimeout: number
-  buffer: Array<any>
-  timeoutFn: NodeJS.Timeout | null
+  account: Account;
+  azBlobClient: BlobService;
+  containerName: string;
+  blobName: string;
+  rotatePeriod: string;
+  eol: string;
+  bufferLogSize: number;
+  syncTimeout: number;
+  buffer: Array<any>;
+  timeoutFn: NodeJS.Timeout | null;
 }
 
 /**
  * Default options for AzureBlob
  */
-type ILoggerDefaults = Pick<IWinstonAzureBlob, 'account' | 'containerName' | 'blobName' | 'eol' | 'bufferLogSize' | 'syncTimeout' | 'rotatePeriod'>
+type ILoggerDefaults = Pick<
+  IWinstonAzureBlob,
+  | "account"
+  | "containerName"
+  | "blobName"
+  | "eol"
+  | "bufferLogSize"
+  | "syncTimeout"
+  | "rotatePeriod"
+>;
 
 /**
  * Data to be logged
  */
-type Data = Record<string, any>
+type Data = Record<string, any>;
 
 /**
  * Default options for constructing logger
  */
 const loggerDefaults: ILoggerDefaults = {
   account: {
-    name: 'YOUR_ACCOUNT_NAME',
-    key: 'YOUR_ACCOUNT_KEY'
+    name: "YOUR_ACCOUNT_NAME",
+    key: "YOUR_ACCOUNT_KEY",
   },
-  containerName: 'YOUR_CONTAINER',
-  blobName: 'YOUR_BLOBNAME',
-  eol: '\n', // End of line character to concatenate log
-  rotatePeriod: '', // moment format to rotate log file
+  containerName: "YOUR_CONTAINER",
+  blobName: "YOUR_BLOBNAME",
+  eol: "\n", // End of line character to concatenate log
+  rotatePeriod: "", // moment format to rotate log file
   // due to limitation of 50K block in azure blob storage we add some params to avoid the limit
   bufferLogSize: -1, // minimum numbers of log before send the block
-  syncTimeout: 0 // maximum time between two push to azure blob
-}
+  syncTimeout: 0, // maximum time between two push to azure blob
+};
 
 //
 // Extend from `winston-transport` to take advantage
@@ -59,121 +75,149 @@ const loggerDefaults: ILoggerDefaults = {
 //
 export class WinstonAzureBlob extends Transport implements IWinstonAzureBlob {
   account!: {
-    name: string
-    key: string
-  }
+    name: string;
+    key: string;
+  };
 
-  azBlobClient: BlobService
-  containerName: string
-  blobName: string
-  rotatePeriod: string
-  eol: string
-  bufferLogSize: number
-  syncTimeout: number
-  buffer: Array<any>
-  timeoutFn: NodeJS.Timeout | null
+  azBlobClient: BlobService;
+  containerName: string;
+  blobName: string;
+  rotatePeriod: string;
+  eol: string;
+  bufferLogSize: number;
+  syncTimeout: number;
+  buffer: Array<any>;
+  timeoutFn: NodeJS.Timeout | null;
 
-  constructor (opts: Transport.TransportStreamOptions & Partial<ILoggerDefaults>) {
-    super(opts)
+  constructor(
+    opts: Transport.TransportStreamOptions & Partial<ILoggerDefaults>
+  ) {
+    super(opts);
 
-    const options = { ...loggerDefaults, ...opts }
+    const options = { ...loggerDefaults, ...opts };
 
     // create az blob client
-    this.azBlobClient = this._createAzClient(options.account)
-    this.containerName = options.containerName
-    this.blobName = options.blobName
-    this.rotatePeriod = options.rotatePeriod
-    this.eol = options.eol
-    this.bufferLogSize = options.bufferLogSize
-    this.syncTimeout = options.syncTimeout
+    this.azBlobClient = this._createAzClient(options.account);
+    this.containerName = options.containerName;
+    this.blobName = options.blobName;
+    this.rotatePeriod = options.rotatePeriod;
+    this.eol = options.eol;
+    this.bufferLogSize = options.bufferLogSize;
+    this.syncTimeout = options.syncTimeout;
     if (this.bufferLogSize > 1 && !this.syncTimeout) {
-      throw new Error('syncTimeout must be set, if there is a bufferLogSize')
+      throw new Error("syncTimeout must be set, if there is a bufferLogSize");
     }
-    this.buffer = []
-    this.timeoutFn = null
+    this.buffer = [];
+    this.timeoutFn = null;
   }
 
-  push (data: Data, callback: async.ErrorCallback<Error>) {
-    if (data) { this.buffer.push(data) }
+  push(data: Data, callback: async.ErrorCallback<Error>) {
+    if (data) {
+      this.buffer.push(data);
+    }
     if (this.bufferLogSize < 1 || this.buffer.length >= this.bufferLogSize) {
-      this._logToAppendBlob(this.buffer, callback) // in this case winston buffer for us
-      this.buffer = []
+      this._logToAppendBlob(this.buffer, callback); // in this case winston buffer for us
+      this.buffer = [];
     } else if (this.syncTimeout && this.timeoutFn === null) {
       this.timeoutFn = setTimeout(() => {
-        const tasks = this.buffer.slice(0)
-        this.buffer = []
-        this.timeoutFn = null // as we can receive push again after timeout we must relaunch the timeout
+        const tasks = this.buffer.slice(0);
+        this.buffer = [];
+        this.timeoutFn = null; // as we can receive push again after timeout we must relaunch the timeout
         this._logToAppendBlob(tasks, () => {
-          debug('Finish to appendblock', tasks.length)
-        })
-      }, this.syncTimeout)
-      callback()
+          debug("Finish to appendblock", tasks.length);
+        });
+      }, this.syncTimeout);
+      callback();
     } else {
       // buffering
-      callback()
+      callback();
     }
   }
 
-  log (info: Data, callback: Function) {
+  log(info: Data, callback: Function) {
     this.push(info, () => {
-      this.emit('logged', info)
-      callback()
-    })
+      this.emit("logged", info);
+      callback();
+    });
   }
 
-  _createAzClient (account_info: Account) {
-    if ('key' in account_info) {
-      return createBlobService(account_info.name, account_info.key)
+  _createAzClient(account_info: Account) {
+    if ("key" in account_info) {
+      return createBlobService(account_info.name, account_info.key);
     }
-    return createBlobServiceWithSas(account_info.host, account_info.sasToken)
+    return createBlobServiceWithSas(account_info.host, account_info.sasToken);
   }
 
-  _chunkString (str: string, len: number) {
-    const size = Math.ceil(str.length / len)
-    const r = Array(size)
-    let offset = 0
+  _chunkString(str: string, len: number) {
+    const size = Math.ceil(str.length / len);
+    const r = Array(size);
+    let offset = 0;
     for (let i = 0; i < size; i++) {
-      r[i] = str.substr(offset, len)
-      offset += len
+      r[i] = str.substr(offset, len);
+      offset += len;
     }
-    return r
+    return r;
   }
 
-  _logToAppendBlob (tasks: Array<Data>, callback: async.ErrorCallback<Error>) {
-    debug('Try to appendblock', tasks.length)
+  _logToAppendBlob(tasks: Array<Data>, callback: async.ErrorCallback<Error>) {
+    debug("Try to appendblock", tasks.length);
     // nothing to log
     if (tasks.length === 0) {
-      return callback()
+      return callback();
     }
-    const azClient = this.azBlobClient
-    const containerName = this.containerName
-    let blobName = this.blobName
-    if (this.rotatePeriod) { blobName = blobName + '.' + moment().format(this.rotatePeriod) }
+    const azClient = this.azBlobClient;
+    const containerName = this.containerName;
+    let blobName = this.blobName;
+    if (this.rotatePeriod) {
+      blobName = blobName + "." + moment().format(this.rotatePeriod);
+    }
 
-    const toSend = tasks.map((item) => item[MESSAGE as unknown as string]).join(this.eol) + this.eol
-    const chunks = this._chunkString(toSend, MAX_APPEND_BLOB_BLOCK_SIZE)
-    debug('Numbers of appendblock needed', chunks.length)
-    debug('Size of chunks', toSend.length)
-    async.eachSeries(chunks, (chunk, nextappendblock) => {
-      azClient.appendBlockFromText(containerName, blobName, chunk, {}, (err: StorageError, _result) => {
-        if (err && err.code) {
-          if (err.code === 'BlobNotFound') {
-            return azClient.createAppendBlobFromText(containerName, blobName, chunk, {}, (err: StorageError, _result) => {
-              if (err) { debug('Error during appendblob creation', err.code) }
-              nextappendblock()
-            })
+    const toSend =
+      tasks.map((item) => item[MESSAGE as unknown as string]).join(this.eol) +
+      this.eol;
+    const chunks = this._chunkString(toSend, MAX_APPEND_BLOB_BLOCK_SIZE);
+    debug("Numbers of appendblock needed", chunks.length);
+    debug("Size of chunks", toSend.length);
+    eachSeries(
+      chunks,
+      (chunk, nextappendblock) => {
+        azClient.appendBlockFromText(
+          containerName,
+          blobName,
+          chunk,
+          {},
+          (err: StorageError, _result) => {
+            if (err && err.code) {
+              if (err.code === "BlobNotFound") {
+                return azClient.createAppendBlobFromText(
+                  containerName,
+                  blobName,
+                  chunk,
+                  {},
+                  (err: StorageError, _result) => {
+                    if (err) {
+                      debug("Error during appendblob creation", err.code);
+                    }
+                    nextappendblock();
+                  }
+                );
+              }
+              if (err.code === "AuthorizationResourceTypeMismatch") {
+                debug(err.message);
+              }
+              debug("Error during appendblob operation", err.code);
+            }
+            nextappendblock();
           }
-          if (err.code === 'AuthorizationResourceTypeMismatch') {
-            debug(err.message)
-          }
-          debug('Error during appendblob operation', err.code)
-        }
-        nextappendblock()
-      })
-    }, callback)
+        );
+      },
+      callback
+    );
   }
 }
 
-export const winstonAzureBlob = (opts: ConstructorParameters<typeof WinstonAzureBlob>[0]) => {
-  return new WinstonAzureBlob(opts)
-}
+export const winstonAzureBlob = (
+  opts: ConstructorParameters<typeof WinstonAzureBlob>[0]
+) => {
+  return new WinstonAzureBlob(opts);
+};
